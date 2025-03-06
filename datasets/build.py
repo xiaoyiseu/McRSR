5e4cf089144519ff33ac84ec63e441df56a9c678
xiaoyiseu <132346882+xiaoyiseu@users.noreply.github.com>
2025-03-06 09:47:06 +0800

    Update build.py

--- datasets/build.py
+++ datasets/build.py
@@ -2,12 +2,6 @@ import logging
 import torch
 import torchvision.transforms as T
 from torch.utils.data import DataLoader
-from datasets.sampler import RandomIdentitySampler
-from datasets.sampler_ddp import RandomIdentitySampler_DDP
-from torch.utils.data.distributed import DistributedSampler
-
-from utils.comm import get_world_size
-
 from .bases import ImageDataset, TextDataset, ImageTextDataset
 
 from .cuhkpedes import CUHKPEDES
