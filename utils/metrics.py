4fee83861ce2be41d70163d88b1af0079abae704
xiaoyiseu <132346882+xiaoyiseu@users.noreply.github.com>
2025-04-30 14:07:14 +0800

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -26,15 +26,12 @@ def rank(similarity, q_pids, g_pids, max_rank=10, get_mAP=True):
     num_rel = matches.sum(1)  # q
     tmp_cmc = matches.cumsum(1)  # q * k
 
-    inp = [tmp_cmc[i][match_row.nonzero()[-1]] / (match_row.nonzero()[-1] + 1.) for i, match_row in enumerate(matches)]
-    mINP = torch.cat(inp).mean() * 100
-
     tmp_cmc = [tmp_cmc[:, i] / (i + 1.0) for i in range(tmp_cmc.shape[1])]
     tmp_cmc = torch.stack(tmp_cmc, 1) * matches
     AP = tmp_cmc.sum(1) / num_rel  # q
     mAP = AP.mean() * 100
 
-    return all_cmc, mAP, mINP, indices
+    return all_cmc, mAP, indices
 
 class Evaluator():
     def __init__(self, img_loader, txt_loader):
@@ -85,21 +82,20 @@ class Evaluator():
 
         similarity = qfeats @ gfeats.t()
 
-        t2i_cmc, t2i_mAP, t2i_mINP, _ = rank(similarity=similarity, q_pids=qids, g_pids=gids, max_rank=10, get_mAP=True)
-        t2i_cmc, t2i_mAP, t2i_mINP = t2i_cmc.numpy(), t2i_mAP.numpy(), t2i_mINP.numpy()
-        table = PrettyTable(["task", "R1", "R5", "R10", "mAP", "mINP"])
-        table.add_row(['t2i', t2i_cmc[0], t2i_cmc[4], t2i_cmc[9], t2i_mAP, t2i_mINP])
+        t2i_cmc, t2i_mAP, _ = rank(similarity=similarity, q_pids=qids, g_pids=gids, max_rank=10, get_mAP=True)
+        t2i_cmc, t2i_mAP = t2i_cmc.numpy(), t2i_mAP.numpy()
+        table = PrettyTable(["task", "R1", "R5", "R10", "mAP"])
+        table.add_row(['t2i', t2i_cmc[0], t2i_cmc[4], t2i_cmc[9], t2i_mAP])
 
         if i2t_metric:
-            i2t_cmc, i2t_mAP, i2t_mINP, _ = rank(similarity=similarity.t(), q_pids=gids, g_pids=qids, max_rank=10, get_mAP=True)
-            i2t_cmc, i2t_mAP, i2t_mINP = i2t_cmc.numpy(), i2t_mAP.numpy(), i2t_mINP.numpy()
-            table.add_row(['i2t', i2t_cmc[0], i2t_cmc[4], i2t_cmc[9], i2t_mAP, i2t_mINP])
+            i2t_cmc, i2t_mAP, _ = rank(similarity=similarity.t(), q_pids=gids, g_pids=qids, max_rank=10, get_mAP=True)
+            i2t_cmc, i2t_mAP = i2t_cmc.numpy(), i2t_mAP.numpy()
+            table.add_row(['i2t', i2t_cmc[0], i2t_cmc[4], i2t_cmc[9], i2t_mAP])
         # table.float_format = '.4'
         table.custom_format["R1"] = lambda f, v: f"{v:.3f}"
         table.custom_format["R5"] = lambda f, v: f"{v:.3f}"
         table.custom_format["R10"] = lambda f, v: f"{v:.3f}"
         table.custom_format["mAP"] = lambda f, v: f"{v:.3f}"
-        table.custom_format["mINP"] = lambda f, v: f"{v:.3f}"
         self.logger.info('\n' + str(table))
         
         return t2i_cmc[0]
